3c730c331e934acf0abbd900367c8f321c174777
Manpreet Singh <mxsingh13@gmail.com>
2026-08-03 17:44:23 -0400

    Clean up

--- infra/backend.tf
+++ infra/backend.tf
@@ -33,9 +33,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "${var.project_name}-tf-locks"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
+    name            = "${var.project_name}-tf-locks"
+    billing_mode    = "PAY_PER_REQUEST"
+    hash_key        = "LockID"
 
     attribute {
       name = "LockID"
